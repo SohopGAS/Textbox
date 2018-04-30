1f34d9ef071cdeafc74aabb0aab3b26325bf87e9
Alan Trope <atrope@users.noreply.github.com>
2018-04-30 13:13:37 +0300

    changing methods to private

--- Textbox/Textbox.h
+++ Textbox/Textbox.h
@@ -8,14 +8,15 @@ private:
 	COORD position;
 	int maxWidth, width;
 	string value;
+	void draw();
+	void gotoxy(int x, int y);
+	void makeRect();
+
 public:
 	Textbox(COORD pos, int maxWidth=30) : position(pos), maxWidth(maxWidth), width(0) {
 		makeRect();
 	};
 	void addValue(CHAR newvalue);
-	void draw();
-	void gotoxy(int x, int y);
-	void makeRect();
 	void delChar();
 	~Textbox() {};
 };

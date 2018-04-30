266e5e6ad84991211a5044c8a2fe974ea3e20bb3
Alan Trope <atrope@users.noreply.github.com>
2018-04-30 12:49:27 +0300

    bugfixing in refreshing text

--- Textbox/Textbox.h
+++ Textbox/Textbox.h
@@ -9,17 +9,10 @@ private:
 	int maxWidth, width;
 	string value;
 public:
-	Textbox(COORD pos, int maxWidth) : position(pos), maxWidth(maxWidth), width(0) {
+	Textbox(COORD pos, int maxWidth=30) : position(pos), maxWidth(maxWidth), width(0) {
 		makeRect();
 	};
-	string getValue() { return value; }
-	void addValue(CHAR newvalue) {
-		if (this->width++ < this->maxWidth) {
-			this->value.push_back(newvalue);
-			draw();
-		}
-	}
-	COORD getPosition() { return position; }
+	void addValue(CHAR newvalue);
 	void draw();
 	void gotoxy(int x, int y);
 	void makeRect();
